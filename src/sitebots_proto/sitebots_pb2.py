2beed2df007c58be0fd83e470108409274384cec
Fabricio Leal <fabricio.leal@sitebots.com>
2019-12-03 14:25:50 +0100

    Added Mission; switched to proto3

--- src/sitebots_proto/sitebots_pb2.py
+++ src/sitebots_proto/sitebots_pb2.py
@@ -18,8 +18,8 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='sitebots.proto',
   package='sitebots',
-  syntax='proto2',
-  serialized_pb=_b('\n\x0esitebots.proto\x12\x08sitebots\"<\n\x08Waypoint\x12\n\n\x02id\x18\x01 \x01(\t\x12\t\n\x01x\x18\x02 \x01(\x02\x12\t\n\x01y\x18\x03 \x01(\x02\x12\x0e\n\x06map_id\x18\x04 \x01(\t\",\n\x04Path\x12$\n\x08waypoint\x18\x01 \x03(\x0b\x32\x12.sitebots.Waypoint\"9\n\x05State\x12\n\n\x02id\x18\x01 \x01(\t\x12\t\n\x01x\x18\x02 \x01(\x02\x12\t\n\x01y\x18\x03 \x01(\x02\x12\x0e\n\x06map_id\x18\x04 \x01(\t')
+  syntax='proto3',
+  serialized_pb=_b('\n\x0esitebots.proto\x12\x08sitebots\"<\n\x08Waypoint\x12\n\n\x02id\x18\x01 \x01(\t\x12\t\n\x01x\x18\x02 \x01(\x02\x12\t\n\x01y\x18\x03 \x01(\x02\x12\x0e\n\x06map_id\x18\x04 \x01(\t\"Z\n\nConnection\x12\n\n\x02id\x18\x01 \x01(\t\x12\x17\n\x0fsource_waypoint\x18\x02 \x01(\t\x12\x17\n\x0ftarget_waypoint\x18\x03 \x01(\t\x12\x0e\n\x06map_id\x18\x04 \x01(\t\"8\n\x04Path\x12\n\n\x02id\x18\x01 \x01(\t\x12$\n\x08waypoint\x18\x02 \x03(\x0b\x32\x12.sitebots.Waypoint\"u\n\x04\x41rea\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0e\n\x06map_id\x18\x02 \x01(\t\x12\x1f\n\x06\x62ounds\x18\x03 \x03(\x0b\x32\x0f.sitebots.Point\x12\"\n\twaypoints\x18\x04 \x03(\x0b\x32\x0f.sitebots.Point\x12\x0c\n\x04name\x18\x05 \x01(\t\"-\n\x05Point\x12\t\n\x01x\x18\x01 \x01(\x02\x12\t\n\x01y\x18\x02 \x01(\x02\x12\x0e\n\x06map_id\x18\x03 \x01(\t\"g\n\x07Vehicle\x12\n\n\x02id\x18\x01 \x01(\t\x12\t\n\x01x\x18\x02 \x01(\x02\x12\t\n\x01y\x18\x03 \x01(\x02\x12\x0e\n\x06map_id\x18\x04 \x01(\t\x12*\n\x0f\x63urrent_mission\x18\x05 \x01(\x0b\x32\x11.sitebots.Mission\"[\n\x0bMissionItem\x12\x1e\n\x04path\x18\x01 \x01(\x0b\x32\x0e.sitebots.PathH\x00\x12\x1e\n\x04\x61rea\x18\x02 \x01(\x0b\x32\x0e.sitebots.AreaH\x00\x42\x0c\n\nitem_oneof\";\n\x07Mission\x12\n\n\x02id\x18\x01 \x01(\t\x12$\n\x05items\x18\x02 \x03(\x0b\x32\x15.sitebots.MissionItemb\x06proto3')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -69,7 +69,7 @@ _WAYPOINT = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto2',
+  syntax='proto3',
   extension_ranges=[],
   oneofs=[
   ],
@@ -78,67 +78,36 @@ _WAYPOINT = _descriptor.Descriptor(
 )
 
 
-_PATH = _descriptor.Descriptor(
-  name='Path',
-  full_name='sitebots.Path',
+_CONNECTION = _descriptor.Descriptor(
+  name='Connection',
+  full_name='sitebots.Connection',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='waypoint', full_name='sitebots.Path.waypoint', index=0,
-      number=1, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  syntax='proto2',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=90,
-  serialized_end=134,
-)
-
-
-_STATE = _descriptor.Descriptor(
-  name='State',
-  full_name='sitebots.State',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='id', full_name='sitebots.State.id', index=0,
+      name='id', full_name='sitebots.Connection.id', index=0,
       number=1, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='x', full_name='sitebots.State.x', index=1,
-      number=2, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=float(0),
+      name='source_waypoint', full_name='sitebots.Connection.source_waypoint', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='y', full_name='sitebots.State.y', index=2,
-      number=3, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=float(0),
+      name='target_waypoint', full_name='sitebots.Connection.target_waypoint', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='map_id', full_name='sitebots.State.map_id', index=3,
+      name='map_id', full_name='sitebots.Connection.map_id', index=3,
       number=4, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
@@ -152,18 +121,315 @@ _STATE = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto2',
+  syntax='proto3',
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=136,
-  serialized_end=193,
+  serialized_start=90,
+  serialized_end=180,
+)
+
+
+_PATH = _descriptor.Descriptor(
+  name='Path',
+  full_name='sitebots.Path',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='id', full_name='sitebots.Path.id', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='waypoint', full_name='sitebots.Path.waypoint', index=1,
+      number=2, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=182,
+  serialized_end=238,
+)
+
+
+_AREA = _descriptor.Descriptor(
+  name='Area',
+  full_name='sitebots.Area',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='id', full_name='sitebots.Area.id', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='map_id', full_name='sitebots.Area.map_id', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='bounds', full_name='sitebots.Area.bounds', index=2,
+      number=3, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='waypoints', full_name='sitebots.Area.waypoints', index=3,
+      number=4, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='name', full_name='sitebots.Area.name', index=4,
+      number=5, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=240,
+  serialized_end=357,
+)
+
+
+_POINT = _descriptor.Descriptor(
+  name='Point',
+  full_name='sitebots.Point',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='x', full_name='sitebots.Point.x', index=0,
+      number=1, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='y', full_name='sitebots.Point.y', index=1,
+      number=2, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='map_id', full_name='sitebots.Point.map_id', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=359,
+  serialized_end=404,
+)
+
+
+_VEHICLE = _descriptor.Descriptor(
+  name='Vehicle',
+  full_name='sitebots.Vehicle',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='id', full_name='sitebots.Vehicle.id', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='x', full_name='sitebots.Vehicle.x', index=1,
+      number=2, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='y', full_name='sitebots.Vehicle.y', index=2,
+      number=3, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='map_id', full_name='sitebots.Vehicle.map_id', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='current_mission', full_name='sitebots.Vehicle.current_mission', index=4,
+      number=5, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=406,
+  serialized_end=509,
+)
+
+
+_MISSIONITEM = _descriptor.Descriptor(
+  name='MissionItem',
+  full_name='sitebots.MissionItem',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='path', full_name='sitebots.MissionItem.path', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='area', full_name='sitebots.MissionItem.area', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+    _descriptor.OneofDescriptor(
+      name='item_oneof', full_name='sitebots.MissionItem.item_oneof',
+      index=0, containing_type=None, fields=[]),
+  ],
+  serialized_start=511,
+  serialized_end=602,
+)
+
+
+_MISSION = _descriptor.Descriptor(
+  name='Mission',
+  full_name='sitebots.Mission',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='id', full_name='sitebots.Mission.id', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='items', full_name='sitebots.Mission.items', index=1,
+      number=2, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=604,
+  serialized_end=663,
 )
 
 _PATH.fields_by_name['waypoint'].message_type = _WAYPOINT
+_AREA.fields_by_name['bounds'].message_type = _POINT
+_AREA.fields_by_name['waypoints'].message_type = _POINT
+_VEHICLE.fields_by_name['current_mission'].message_type = _MISSION
+_MISSIONITEM.fields_by_name['path'].message_type = _PATH
+_MISSIONITEM.fields_by_name['area'].message_type = _AREA
+_MISSIONITEM.oneofs_by_name['item_oneof'].fields.append(
+  _MISSIONITEM.fields_by_name['path'])
+_MISSIONITEM.fields_by_name['path'].containing_oneof = _MISSIONITEM.oneofs_by_name['item_oneof']
+_MISSIONITEM.oneofs_by_name['item_oneof'].fields.append(
+  _MISSIONITEM.fields_by_name['area'])
+_MISSIONITEM.fields_by_name['area'].containing_oneof = _MISSIONITEM.oneofs_by_name['item_oneof']
+_MISSION.fields_by_name['items'].message_type = _MISSIONITEM
 DESCRIPTOR.message_types_by_name['Waypoint'] = _WAYPOINT
+DESCRIPTOR.message_types_by_name['Connection'] = _CONNECTION
 DESCRIPTOR.message_types_by_name['Path'] = _PATH
-DESCRIPTOR.message_types_by_name['State'] = _STATE
+DESCRIPTOR.message_types_by_name['Area'] = _AREA
+DESCRIPTOR.message_types_by_name['Point'] = _POINT
+DESCRIPTOR.message_types_by_name['Vehicle'] = _VEHICLE
+DESCRIPTOR.message_types_by_name['MissionItem'] = _MISSIONITEM
+DESCRIPTOR.message_types_by_name['Mission'] = _MISSION
 
 Waypoint = _reflection.GeneratedProtocolMessageType('Waypoint', (_message.Message,), dict(
   DESCRIPTOR = _WAYPOINT,
@@ -172,6 +438,13 @@ Waypoint = _reflection.GeneratedProtocolMessageType('Waypoint', (_message.Messag
   ))
 _sym_db.RegisterMessage(Waypoint)
 
+Connection = _reflection.GeneratedProtocolMessageType('Connection', (_message.Message,), dict(
+  DESCRIPTOR = _CONNECTION,
+  __module__ = 'sitebots_pb2'
+  # @@protoc_insertion_point(class_scope:sitebots.Connection)
+  ))
+_sym_db.RegisterMessage(Connection)
+
 Path = _reflection.GeneratedProtocolMessageType('Path', (_message.Message,), dict(
   DESCRIPTOR = _PATH,
   __module__ = 'sitebots_pb2'
@@ -179,12 +452,40 @@ Path = _reflection.GeneratedProtocolMessageType('Path', (_message.Message,), dic
   ))
 _sym_db.RegisterMessage(Path)
 
-State = _reflection.GeneratedProtocolMessageType('State', (_message.Message,), dict(
-  DESCRIPTOR = _STATE,
+Area = _reflection.GeneratedProtocolMessageType('Area', (_message.Message,), dict(
+  DESCRIPTOR = _AREA,
   __module__ = 'sitebots_pb2'
-  # @@protoc_insertion_point(class_scope:sitebots.State)
+  # @@protoc_insertion_point(class_scope:sitebots.Area)
   ))
-_sym_db.RegisterMessage(State)
+_sym_db.RegisterMessage(Area)
+
+Point = _reflection.GeneratedProtocolMessageType('Point', (_message.Message,), dict(
+  DESCRIPTOR = _POINT,
+  __module__ = 'sitebots_pb2'
+  # @@protoc_insertion_point(class_scope:sitebots.Point)
+  ))
+_sym_db.RegisterMessage(Point)
+
+Vehicle = _reflection.GeneratedProtocolMessageType('Vehicle', (_message.Message,), dict(
+  DESCRIPTOR = _VEHICLE,
+  __module__ = 'sitebots_pb2'
+  # @@protoc_insertion_point(class_scope:sitebots.Vehicle)
+  ))
+_sym_db.RegisterMessage(Vehicle)
+
+MissionItem = _reflection.GeneratedProtocolMessageType('MissionItem', (_message.Message,), dict(
+  DESCRIPTOR = _MISSIONITEM,
+  __module__ = 'sitebots_pb2'
+  # @@protoc_insertion_point(class_scope:sitebots.MissionItem)
+  ))
+_sym_db.RegisterMessage(MissionItem)
+
+Mission = _reflection.GeneratedProtocolMessageType('Mission', (_message.Message,), dict(
+  DESCRIPTOR = _MISSION,
+  __module__ = 'sitebots_pb2'
+  # @@protoc_insertion_point(class_scope:sitebots.Mission)
+  ))
+_sym_db.RegisterMessage(Mission)
 
 
 # @@protoc_insertion_point(module_scope)
